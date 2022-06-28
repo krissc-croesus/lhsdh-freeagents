8b4e40eb8626693a5cc9c7a3a54d5fef759a7456
Kriss Cardenas <kriss_96@hotmail.com>
2022-06-28 14:36:56 -0400

    Migration to Angular 11

--- src/app/all-free-agents/all-free-agents.component.spec.ts
+++ src/app/all-free-agents/all-free-agents.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AllFreeAgentsComponent } from './all-free-agents.component';
 
@@ -6,7 +6,7 @@ describe('AllFreeAgentsComponent', () => {
   let component: AllFreeAgentsComponent;
   let fixture: ComponentFixture<AllFreeAgentsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AllFreeAgentsComponent ]
     })
