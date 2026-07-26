bb1a2fe67cb8d5e61cb2d543b0c1d21a3daecd8c
Kriss Cardenas <kriss_96@hotmail.com>
2026-07-26 03:55:17 -0400

    fix: cognito erros

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -3,7 +3,6 @@
 
 const awsmobile = {
   "aws_project_region": "us-east-2",
-  "aws_cognito_identity_pool_id": "us-east-2:806616824497",
   "aws_cognito_region": "us-east-2",
   "aws_user_pools_id": "us-east-2_TQAabE2yt",
   "aws_user_pools_web_client_id": "b8ecca0e3ns7kg0l63kigth30",
